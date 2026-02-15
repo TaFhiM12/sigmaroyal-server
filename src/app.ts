9d4acc6941b193f6a8956309c4f4db842c6b508c
Tanvir Mahtab Tafhim <tafhim000001@gmail.com>
2026-02-15 16:59:03 +0600

    jjk

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Application, Request, Response} from 'express';
 import { projectRoutes } from './module/project/project.route';
+import cors from 'cors';
 
 const app: Application = express();
-
+app.use(cors(
+    {
+        origin: 'https://sigmaroyal-client.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.json());
 
 app.use('/api/v1/projects', projectRoutes);
